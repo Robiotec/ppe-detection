aa9ee96293260a193890112795e8254690d284a1
tyrerodr-robiotec <trodriguez@grupominerobonanza.com>
2025-10-31 21:05:55 -0500

    feat: add video with inference running

--- tools/test_video.py
+++ tools/test_video.py
@@ -1,30 +1,31 @@
 import cv2
+import imageio
 from ultralytics import YOLO
 
 def main():
     model_path = "/home/robiotec/Documents/ppe-detection/src/PPE_yolo11n/weights/best.pt"
-    video_path = "/home/robiotec/Documents/ppe-detection/src/test.mp4"  
+    video_path = "/home/robiotec/Documents/ppe-detection/src/test.mp4"
 
     model = YOLO(model_path)
-
     cap = cv2.VideoCapture(video_path)
+
     if not cap.isOpened():
         print(f"No se pudo abrir el video: {video_path}")
         return
 
-    frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    fps = int(cap.get(cv2.CAP_PROP_FPS))
-    
-    output_path = video_path.rsplit('.', 1)[0] + '_detected.mp4'
-    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-    out = cv2.VideoWriter(output_path, fourcc, fps, (frame_width, frame_height))
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    if fps == 0 or fps is None:
+        fps = 30 
 
-    print("Reproduciendo video...")
-    print(f"El video se guardará en: {output_path}")
-    window_name = "Resultados de Detección de PPE"
+    output_path = video_path.rsplit('.', 1)[0] + "_detected.mp4"
+
+    print("Procesando detecciones con YOLO...")
+
+    window_name = "Detección de EPP"
     cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
     cv2.resizeWindow(window_name, 1280, 720)
+
+    writer = imageio.get_writer(output_path, fps=fps, codec="libx264", quality=8)
 
     try:
         while True:
@@ -36,22 +37,20 @@ def main():
             results = model.predict(frame, conf=0.5)
             result_image = results[0].plot(conf=False)
 
-            out.write(result_image)
+            rgb_frame = cv2.cvtColor(result_image, cv2.COLOR_BGR2RGB)
+            writer.append_data(rgb_frame)
 
             cv2.imshow(window_name, result_image)
-
             key = cv2.waitKey(1) & 0xFF
             if key == ord('q'):
                 break
 
     except KeyboardInterrupt:
-        pass
+        print("Interrupción manual detectada.")
     finally:
         cap.release()
-        out.release()
+        writer.close()
         cv2.destroyAllWindows()
-        print(f"Video guardado en: {output_path}")
-        print("Terminando aplicación...")
 
 if __name__ == "__main__":
     main()
